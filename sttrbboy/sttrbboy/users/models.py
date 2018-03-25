bf025be8e4e28d6a15892adfe9956d4e40de4331
ceidsmoe <ceidsmoe@uchicago.edu>
2018-03-24 23:37:12 -0500

    create associated profile on log in

--- sttrbboy/sttrbboy/users/models.py
+++ sttrbboy/sttrbboy/users/models.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from django.contrib.auth.models import User
 from sttrbboy import settings
 
 
@@ -11,3 +11,9 @@ class Profile(models.Model):
 	gender_pronouns = models.CharField(max_length=128)
 	#phone_number = PhoneNumberField(blank=True)
 	user = models.OneToOneField(settings.AUTH_USER_MODEL)
+
+def get_or_create_profile(sender, **kwargs):
+	if not kwargs.get('raw'):
+		profile, created = Profile.objects.get_or_create(user=kwargs['instance'])
+
+models.signals.post_save.connect(get_or_create_profile, sender=User)
